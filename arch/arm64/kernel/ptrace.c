6fa230e983a9d61ee69fdbc024d4a910fe9fef29
Colin Lee <colin.lee@mediatek.com>
2019-04-25 10:12:07 +0800

    [ALPS04382002] [Google Security Patch][CVE-2019-2054]seccomp: fix ptrace vulnerability
    [Solution]
      The fix is designed to fix the ptrace related security hole in seccomp.
      It contains following patches for ARM platform:
    
      0aa0a57:[PATCH] UPSTREAM: seccomp: recheck the syscall after RET_TRACE
      351181c:[PATCH] BACKPORT: seccomp: Add a seccomp_data parameter
              secure_computing()
      68fd101:[PATCH] UPSTREAM: arm64/ptrace: run seccomp after ptrace
      a26c91b:[PATCH] UPSTREAM: seccomp: Fix tracer exit notifications during fatal
              signals
      c1e7c22:[PATCH] UPSTREAM: seccomp: remove 2-phase API
      d5ea505:[PATCH] UPSTREAM: arm/ptrace: run seccomp after ptrace
    
    MTK-Commit-Id: 4bfa707cf22f32334677be15206a2f678a794214
    
    Change-Id: I7ce23125306b132a60e5b26da769d0eada2d0895
    Signed-off-by: Colin Lee <colin.lee@mediatek.com>
    CR-Id: ALPS04382002
    Feature: Secure Boot
    Signed-off-by: Ayush <ayush@disroot.org>
    
    # Conflicts:
    #	arch/arm64/kernel/ptrace.c
    #	include/linux/seccomp.h
    #	kernel/seccomp.c
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -1254,10 +1254,11 @@ static void tracehook_report_syscall(struct pt_regs *regs,
 
 asmlinkage int syscall_trace_enter(struct pt_regs *regs)
 {
+
 	if (test_thread_flag(TIF_SYSCALL_TRACE))
 		tracehook_report_syscall(regs, PTRACE_SYSCALL_ENTER);
 
-	/* Do the secure computing check first; failures should be fast. */
+	/* Do the secure computing after ptrace; failures should be fast. */
 	if (secure_computing(NULL) == -1)
 		return -1;
 
