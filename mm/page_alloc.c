14ca1d176de8c9d54a97bc4bebfb068c6d201b88
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-18 22:52:50 -0800

    mm: Stop kswapd early when nothing's waiting for it to free pages
    Keeping kswapd running when all the failed allocations that invoked it
    are satisfied incurs a high overhead due to unnecessary page eviction
    and writeback, as well as spurious VM pressure events to various
    registered shrinkers. When kswapd doesn't need to work to make an
    allocation succeed anymore, stop it prematurely to save resources.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: CyberJalagam <rbinternationalnetwork@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3110,6 +3110,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	enum migrate_mode migration_mode = MIGRATE_ASYNC;
 	bool deferred_compaction = false;
 	int contended_compaction = COMPACT_CONTENDED_NONE;
+	pg_data_t *pgdat = ac->preferred_zone->zone_pgdat;
 	bool woke_kswapd = false;
 
 	/*
@@ -3142,7 +3143,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 retry:
 	if (gfp_mask & __GFP_KSWAPD_RECLAIM) {
 		if (!woke_kswapd) {
-			atomic_long_inc(&kswapd_waiters);
+			atomic_inc(&pgdat->kswapd_waiters);
 			woke_kswapd = true;
 		}
 		wake_all_kswapds(order, ac);
@@ -3300,7 +3301,7 @@ noretry:
 nopage:
 got_pg:
 	if (woke_kswapd)
-		atomic_long_dec(&kswapd_waiters);
+		atomic_dec(&pgdat->kswapd_waiters);
 	if (!page)
 		warn_alloc_failed(gfp_mask, order, NULL);
 	return page;
@@ -5364,6 +5365,7 @@ static void __paginginit free_area_init_core(struct pglist_data *pgdat)
 	init_waitqueue_head(&pgdat->kswapd_wait);
 	init_waitqueue_head(&pgdat->pfmemalloc_wait);
 	pgdat_page_ext_init(pgdat);
+	pgdat->kswapd_waiters = (atomic_t)ATOMIC_INIT(0);
 
 	for (j = 0; j < MAX_NR_ZONES; j++) {
 		struct zone *zone = pgdat->node_zones + j;
