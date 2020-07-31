18629aa725f181ee258372dc3f659927881260b4
Ayush <ayush@disroot.org>
2020-07-31 18:03:56 +0000

    Changes in compile script
    Also
    *add .gitignore
    *remove unused scripts
    
    Signed-off-by: Ayush <ayush@disroot.org>

--- clang_compile.sh
+++ clang_compile.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-echo "Cloning dependencies"
+echo "Cloning dependencies if they don't exist...."
+
+if [ ! -d "./clang"]
 git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5407736 clang
+
+if [ ! -d "./gcc32"]
 git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
+
+if [ ! -d "./gcc"]
 git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
+
+if [ ! -d "./AnyKernel"]
 git clone https://gitlab.com/Baibhab34/AnyKernel3.git -b rm1 --depth=1 AnyKernel
 echo "Done"
+
+
 KERNEL_DIR=$(pwd)
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
 TANGGAL=$(date +"%Y%m%d-%H")
@@ -12,8 +22,8 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export KBUILD_BUILD_USER=baibhab
-export KBUILD_BUILD_HOST=azure
+export KBUILD_BUILD_USER=ayush
+export KBUILD_BUILD_HOST=gcp
 
 # Compile plox
 function compile() {
@@ -24,14 +34,28 @@ function compile() {
                     CLANG_TRIPLE=aarch64-linux-gnu- \
                     CROSS_COMPILE=aarch64-linux-android- \
                     CROSS_COMPILE_ARM32=arm-linux-androideabi-
+SUCCESS=$?
+	if [ $SUCCESS -eq 0 ]
+        	then
+		echo "Compilation successful..."
+        	echo "Image.gz-dt can be found at out/arch/arm64/boot/Image.gz-dtb"
+    		cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+	else
+		echo "Compilation failed..check build logs for errors"
+	fi
 
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 function zipping() {
+    echo "Creating a flashable zip....."
     cd AnyKernel || exit 1
-    zip -r9 Stormbreaker-RMX1801-HMP-${TANGGAL}.zip *
+    zip -r9 Stormbreaker-CPH1859-${TANGGAL}.zip * > /dev/null 2>&1
     cd ..
+    echo "Zip stored at AnyKernel/Stormbreaker-CPH1859-${TANGGAL}.zip"
 }
 compile
-zipping
+
+if [ $SUCCESS -eq 0 ]
+then
+	zipping
+fi
