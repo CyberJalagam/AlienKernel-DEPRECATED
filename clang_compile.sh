b91d90bc429916a153dc7aebac06744af6ebe566
Ayush <ayush@disroot.org>
2020-08-04 21:42:19 +0000

    Use stock ion.c
    Signed-off-by: Ayush <ayush@disroot.org>

--- clang_compile.sh
+++ clang_compile.sh
@@ -2,16 +2,25 @@
 echo "Cloning dependencies if they don't exist...."
 
 if [ ! -d clang ]
+then
 git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5407736 clang
+fi
 
 if [ ! -d gcc32 ]
+then
 git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
+fi
 
 if [ ! -d gcc ]
+then
 git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
+fi
 
 if [ ! -d AnyKernel ]
+then
 git clone https://gitlab.com/Baibhab34/AnyKernel3.git -b rm1 --depth=1 AnyKernel
+fi
+
 echo "Done"
 
 
