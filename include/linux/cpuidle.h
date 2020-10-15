83521edf5698a447fe2527a929c6038e4e920576
CyberJalagam <rbinternationalnetwork@gmail.com>
2020-10-15 09:48:48 +0530

    Revert "cpuidle: Allow enforcing deepest idle state selection"
    This reverts commit e76470646a5e96eec3ef248aac4c9cd823477bb7.
    as it is causing issues

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -74,7 +74,6 @@ struct cpuidle_driver_kobj;
 struct cpuidle_device {
 	unsigned int		registered:1;
 	unsigned int		enabled:1;
-	unsigned int		use_deepest_state:1;
 	unsigned int		cpu;
 
 	int			last_residency;
@@ -190,13 +189,11 @@ static inline struct cpuidle_driver *cpuidle_get_cpu_driver(
 	struct cpuidle_device *dev) {return NULL; }
 #endif
 
-#ifdef CONFIG_CPU_IDLE
-
+#if defined(CONFIG_CPU_IDLE) && defined(CONFIG_SUSPEND)
 extern int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 				      struct cpuidle_device *dev);
 extern int cpuidle_enter_freeze(struct cpuidle_driver *drv,
 				struct cpuidle_device *dev);
-extern void cpuidle_use_deepest_state(bool enable);				
 #else
 static inline int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 					     struct cpuidle_device *dev)
@@ -204,9 +201,6 @@ static inline int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 static inline int cpuidle_enter_freeze(struct cpuidle_driver *drv,
 				       struct cpuidle_device *dev)
 {return -ENODEV; }
-static inline void cpuidle_use_deepest_state(bool enable)
-{
-}
 #endif
 
 /* kernel/sched/idle.c */
