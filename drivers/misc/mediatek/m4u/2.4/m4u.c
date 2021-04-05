2eb79e37ccae7c23299eee96e54c6304d49b096e
JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>
2021-04-05 11:26:09 +0000

    TreeWide: Fix build errors

--- drivers/misc/mediatek/m4u/2.4/m4u.c
+++ drivers/misc/mediatek/m4u/2.4/m4u.c
@@ -410,11 +410,6 @@ static int m4u_fill_sgtable_user(struct vm_area_struct *vma, unsigned long va, i
 	int i, ret;
 	struct scatterlist *sg = *pSg;
 	struct page *pages;
-	
-	 unsigned int flags = 0;
-
-        if (vma->vm_flags & VM_WRITE)
-                flags |= FOLL_WRITE;
 
 	unsigned int flags = 0;
 
--- include/uapi/linux/android/binder.h
+++ include/uapi/linux/android/binder.h
@@ -321,6 +321,11 @@ struct binder_transaction_data {
 	} data;
 };
 
+struct binder_transaction_data_secctx {
+	struct binder_transaction_data transaction_data;
+	binder_uintptr_t secctx;
+};
+
 struct binder_transaction_data_sg {
 
 	struct binder_transaction_data transaction_data;
